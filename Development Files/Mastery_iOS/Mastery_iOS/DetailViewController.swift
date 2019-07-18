c053dd6857f0dfd2a95b5176ed8769cd35903aef
Marina <mmcpeak@live.ca>
2019-07-17 20:05:57 -0700

    Created Xcode project and Sketch file.

---
+++ Development Files/Mastery_iOS/Mastery_iOS/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Mastery_iOS
+//
+//  Created by Marina Mona June McPeak on 2019-07-17.
+//  Copyright © 2019 Marina Mona June McPeak. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
