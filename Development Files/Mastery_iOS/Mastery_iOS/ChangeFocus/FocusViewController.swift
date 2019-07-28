17009299ed8bb644353ccad4c0f6b7baee23f2cf
Cam McLeod <cammcleod3@gmail.com>
2019-07-28 13:47:02 -0700

    Focus View Controller created in Today View Storyboard Flow

--- Development Files/Mastery_iOS/Mastery_iOS/ChangeFocus/FocusViewController.swift
+++ Development Files/Mastery_iOS/Mastery_iOS/ChangeFocus/FocusViewController.swift
@@ -7,13 +7,47 @@
 //
 
 import UIKit
+import CoreData
 
 class FocusViewController: UIViewController {
+    
+    var taskID : UUID?
+    var task = Task()
+    var taskPredicate: NSPredicate?
 
+    @IBOutlet weak var focusTestLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        // TEST TASK
+        let task1 = Task(context: PersistenceService.context)
+        task1.name = "Be Cool"
+        task1.taskDescription = "I just want to be everyone's friend and like have super cool parteeees."
+        task1.isComplete = false
+        task1.daysAvailable = Array.init(repeating: true, count: 7)
+        task1.deadline = NSDate(timeIntervalSinceNow: 10000)
+        task1.priority = Int16(7)
+        task1.id = UUID()
+        PersistenceService.saveContext()
 
         // Do any additional setup after loading the view.
+        // UNCOMMENT TO UNLEASH FURY
+        guard let id = self.taskID else {
+            print("id not UIID")
+            return
+        }
+            
+        let fetchRequest = NSFetchRequest<Task>(entityName: "Task")
+        fetchRequest.predicate = NSPredicate(format: "id == %@", argumentArray: [id])
+        do {
+            let taskFromID =  try PersistenceService.context.fetch(fetchRequest)[0]
+            self.task = taskFromID
+        } catch {
+            print("Oh no, there is no data to load")
+        }
+        focusTestLabel.text = self.task.name
     }
     
 
