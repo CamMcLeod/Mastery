802c180f335ec7944f5311e90d3d9766c4d90a20
Marina <mmcpeak@live.ca>
2019-07-27 17:26:22 -0700

    Created classes for UIViews. Linked storyboard UIViews to xibs and linked xibs to View classes.

---
+++ Development Files/Mastery_iOS/Mastery_iOS/UI Views/TaskIconWithLabelView.swift
@@ -0,0 +1,13 @@
+//
+//  taskIconWithLabelView.swift
+//  Mastery_iOS
+//
+//  Created by Marina Mona June McPeak on 2019-07-27.
+//  Copyright © 2019 Marina Mona June McPeak. All rights reserved.
+//
+
+import UIKit
+
+class TaskIconWithLabelView : UIView {
+    
+}
