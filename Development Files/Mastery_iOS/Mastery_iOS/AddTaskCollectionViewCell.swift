ad93e7738419f6837a5314282adadd3b1942ac41
Ekam Singh Dhaliwal <ekamsingh123.es@gmail.com>
2019-07-30 12:39:24 -0700

    added missing file

---
+++ Development Files/Mastery_iOS/Mastery_iOS/AddTaskCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  AddTaskCollectionViewCell.swift
+//  Mastery_iOS
+//
+//  Created by Ekam Singh Dhaliwal on 2019-07-28.
+//  Copyright © 2019 Marina Mona June McPeak. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var addTask: UILabel!
+    
+}
