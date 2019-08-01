5fb3e0697ece96648175f8fbd9f87d5e490e544c
Marina <mmcpeak@live.ca>
2019-07-31 21:18:11 -0700

    Adjusted storyboards for new goal.

--- Development Files/Mastery_iOS/Mastery_iOS/Create New Goal/GoalDescriptionTableViewCell.swift
+++ Development Files/Mastery_iOS/Mastery_iOS/Create New Goal/GoalDescriptionTableViewCell.swift
@@ -17,6 +17,7 @@ class GoalDescriptionTableViewCell: UITableViewCell, UITextViewDelegate {
     
     var delegate: GoalDescriptionCellDelegate?
     
+    @IBOutlet weak var descriptionTitle: UILabel!
     @IBOutlet weak var goalDescription: UITextView!
 
     override func awakeFromNib() {
